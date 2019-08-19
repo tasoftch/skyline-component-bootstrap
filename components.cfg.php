4287a0b8d55cd6741965227241c1895632cf1069
Thomas Abplanalp <info@tasoft.ch>
2019-08-19 18:10:34 +0200

    Initial Commit

--- components.cfg.php
+++ components.cfg.php
@@ -26,7 +26,7 @@ use Skyline\Component\Config\JavaScriptPostLoadComponent;
 
 return [
     'Bootstrap' => [
-        'js' => new JavaScriptPostLoadComponent('https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/js/bootstrap.bundle.min.js', "sha384-VoPFvGr9GxhDT3n8vqqZ46twP5lgex+raTCfICQy73NLhN7ZqSfCtfSn4mLA2EFA"),
-        "css" => new CSSComponent("https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css", 'all', 'sha384-PDle/QlgIONtM1aqA2Qemk5gPOE7wFq8+Em+G/hmo5Iq0CCmYZLv3fVRDJ4MMwEA')
+        'js' => new JavaScriptPostLoadComponent('https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js', "sha384-feJI7QwhOS+hwpX2zkaeJQjeiwlhOP+SdQDqhgvvo1DsjtiSQByFdThsxO669S2D"),
+        "css" => new CSSComponent("https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css", 'all', 'sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm')
     ]
 ];
